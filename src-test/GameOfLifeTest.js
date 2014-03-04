dbf3a587e2fd1ce3528260d3f45202dc213b52dc
Douglas Hellinger <doug.hellinger@hotmail.com>
2014-03-04 17:49:59 +0800

    Tests for a row and column of 3 live cells. Code evolving from returning constant coordinates to returning an expression representing the 'middle' one.

--- src-test/GameOfLifeTest.js
+++ src-test/GameOfLifeTest.js
@@ -39,15 +39,52 @@ GolTest.prototype.test_afterTick_worldWithTwoAdjacentLiveCells_becomesDead = fun
 
 /**
  * @before [*][*]
+ * 		   [*][*]
+ * @after  [*][*]
+ * 		   [*][*]
+ */
+GolTest.prototype.test_afterTick_allCellsWithThreeLiveNeighboursSurvive = function() {
+	  var world = new myapp.World(liveCellCoordinates=[[0,0], [1,0], [0,1], [1,1]]);
+	  liveCellCoordinates = world.tick();
+	  assertEquals([[0,0], [1,0], [0,1], [1,1]], liveCellCoordinates);
+};
+
+/**
+ * @before [*][*][*]
+ * @after  [ ][*][ ]
+ */
+GolTest.prototype.test_afterTick_threeLiveCellsInARow_middleSurvives = function() {
+	  var world = new myapp.World(liveCellCoordinates=[[0,0], [1,0], [2,0]]);
+	  liveCellCoordinates = world.tick();
+	  assertEquals([[1,0]], liveCellCoordinates);
+};
+
+/**
+ * @before [*][ ][ ]
+ * 		   [*][ ][ ]
+ * 		   [*][ ][ ]
+ * 
+ * @after  [ ][ ][ ]
+ * 		   [*][ ][ ]
+ * 		   [ ][ ][ ]
+ */
+GolTest.prototype.test_afterTick_threeLiveCellsInAColumn_middleSurvives = function() {
+	  var world = new myapp.World(liveCellCoordinates=[[0,0], [0,1], [0,2]]);
+	  liveCellCoordinates = world.tick();
+	  assertEquals([[0,1]], liveCellCoordinates);
+};
+
+/**
+ * @before [*][*]
  * 		   [*][ ]
  * @after  [*][ ]
- * 		   [ ][ ]
+ * 		   [ ][*]
  */
-GolTest.prototype.test_afterTick_liveCellWithTwoLiveNeighbours_survives = function() {
-	  var world = new myapp.World(liveCellCoordinates=[[0,0], [1,0], [0,1]]);
-	  liveCellCoordinates = world.tick();
-	  assertEquals([0,0], liveCellCoordinates);
-};
+//GolTest.prototype.test_afterTick_liveCellWithTwoLiveNeighbours_survives = function() {
+//	  var world = new myapp.World(liveCellCoordinates=[[0,0], [1,0], [0,1]]);
+//	  liveCellCoordinates = world.tick();
+//	  assertEquals([[0,0], [1,1]], liveCellCoordinates);
+//};
 
 //GolTest.prototype.test_afterTick_anotherliveCellWithTwoLiveNeighbours_survives = function() {
 //	  var world = new myapp.World(liveCellCoordinates=[[0,0], [1,0], [1,1]]);
