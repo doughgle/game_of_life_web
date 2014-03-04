0706aa756ac59a264e2567790ba6650c4a03752d
Douglas Hellinger <doug.hellinger@hotmail.com>
2014-03-04 17:24:00 +0800

    Commented test with visual representations of the world/grid/board. Backtracked on test which forces big steps.

--- src-test/GameOfLifeTest.js
+++ src-test/GameOfLifeTest.js
@@ -1,32 +1,57 @@
 GolTest = TestCase("GolTest");
 
+/**
+ * Key:
+ * Dead cell [ ]
+ * Live cell [*]
+ */
+
+/**
+ * @before [ ]
+ * @after  [ ]
+ */
 GolTest.prototype.test_afterTick_worldWithOneDeadCell_remainsDead = function() {
+  
   var world = new myapp.World(liveCellCoordinates=[]);
   liveCellCoordinates = world.tick();
   assertEquals([], liveCellCoordinates);
 };
 
+/**
+ * @before [*]
+ * @after  [ ]
+ */
 GolTest.prototype.test_afterTick_worldWithOneLiveCell_becomesDead = function() {
 	  var world = new myapp.World(liveCellCoordinates=[[0,0]]);
 	  liveCellCoordinates = world.tick();
 	  assertEquals([], liveCellCoordinates);
 };
 
+/**
+ * @before [*][*]
+ * @after  [ ][ ]
+ */
 GolTest.prototype.test_afterTick_worldWithTwoAdjacentLiveCells_becomesDead = function() {
 	  var world = new myapp.World(liveCellCoordinates=[[0,0], [1,0]]);
 	  liveCellCoordinates = world.tick();
 	  assertEquals([], liveCellCoordinates);
 };
 
+/**
+ * @before [*][*]
+ * 		   [*][ ]
+ * @after  [*][ ]
+ * 		   [ ][ ]
+ */
 GolTest.prototype.test_afterTick_liveCellWithTwoLiveNeighbours_survives = function() {
 	  var world = new myapp.World(liveCellCoordinates=[[0,0], [1,0], [0,1]]);
 	  liveCellCoordinates = world.tick();
 	  assertEquals([0,0], liveCellCoordinates);
 };
 
-GolTest.prototype.test_afterTick_anotherliveCellWithTwoLiveNeighbours_survives = function() {
-	  var world = new myapp.World(liveCellCoordinates=[[0,0], [1,0], [1,1]]);
-	  liveCellCoordinates = world.tick();
-	  assertEquals([1,0], liveCellCoordinates);
-};
+//GolTest.prototype.test_afterTick_anotherliveCellWithTwoLiveNeighbours_survives = function() {
+//	  var world = new myapp.World(liveCellCoordinates=[[0,0], [1,0], [1,1]]);
+//	  liveCellCoordinates = world.tick();
+//	  assertEquals([1,0], liveCellCoordinates);
+//};
 
