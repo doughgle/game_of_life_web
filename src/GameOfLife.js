c53ca607ed4c81ea59226c1c0517350f2238ed15
Douglas Hellinger <doug.hellinger@hotmail.com>
2014-03-07 22:07:16 +0800

    Changed the scope of certain functions to make the public interface clearer. Not sure if this the best way to do private methods in Javascript.

--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -10,8 +10,38 @@ myapp = {};
 myapp.World = function(dimension, liveCellCoordinatesArray) {
 	this.dimension = dimension;
 	this.liveCellCoordinates = liveCellCoordinatesArray;
+	
+	function countLiveNeighbours(x, y) {
+		var liveNeighbours = 0;
+		
+		if(isCellAlive(x-1, y-1)) liveNeighbours++;
+		if(isCellAlive(x,   y-1)) liveNeighbours++;
+		if(isCellAlive(x+1, y-1)) liveNeighbours++;
+		if(isCellAlive(x-1, y)) liveNeighbours++;
+		if(isCellAlive(x+1, y)) liveNeighbours++;
+		if(isCellAlive(x-1, y+1)) liveNeighbours++;
+		if(isCellAlive(x,   y+1)) liveNeighbours++;
+		if(isCellAlive(x+1, y+1)) liveNeighbours++;
+		
+//		jstestdriver.console.log(">>> liveNeighbours of ", x, y, ": ", liveNeighbours);
+		return liveNeighbours;
+	};
+
+	function isCellAlive(x, y) {
+		for(var cell in this.liveCellCoordinates) {		
+			if(x === this.liveCellCoordinates[cell][0] && y === this.liveCellCoordinates[cell][1]) {
+				return true;
+			}
+		}
+		return false;
+	};
+
 };
 
+/**
+ * 
+ * @returns {Array} of live cells after tick.
+ */
 myapp.World.prototype.tick = function() {
 	var nextGenerationOfLiveCells = [];
 	
@@ -19,8 +49,8 @@ myapp.World.prototype.tick = function() {
 	for(var y=0; y<this.dimension; y++) {
 		for(var x=0; x<this.dimension; x++) {
 
-			var liveNeighbours = this.countLiveNeighbours(x, y);
-			if(this.isCellAlive(x, y)) {
+			var liveNeighbours = countLiveNeighbours(x, y);
+			if(isCellAlive(x, y)) {
 				if(liveNeighbours === 2 || liveNeighbours === 3) {
 					//    survives
 					nextGenerationOfLiveCells.push([x,y]);
@@ -32,29 +62,4 @@ myapp.World.prototype.tick = function() {
 	}
 	
 	return nextGenerationOfLiveCells;
-};
-
-myapp.World.prototype.countLiveNeighbours = function(x, y) {
-	var liveNeighbours = 0;
-	
-	if(this.isCellAlive(x-1, y-1)) liveNeighbours++;
-	if(this.isCellAlive(x,   y-1)) liveNeighbours++;
-	if(this.isCellAlive(x+1, y-1)) liveNeighbours++;
-	if(this.isCellAlive(x-1, y)) liveNeighbours++;
-	if(this.isCellAlive(x+1, y)) liveNeighbours++;
-	if(this.isCellAlive(x-1, y+1)) liveNeighbours++;
-	if(this.isCellAlive(x,   y+1)) liveNeighbours++;
-	if(this.isCellAlive(x+1, y+1)) liveNeighbours++;
-	
-//	jstestdriver.console.log(">>> liveNeighbours of ", x, y, ": ", liveNeighbours);
-	return liveNeighbours;
-};
-
-myapp.World.prototype.isCellAlive = function(x, y) {
-	for(var cell in this.liveCellCoordinates) {		
-		if(x === this.liveCellCoordinates[cell][0] && y === this.liveCellCoordinates[cell][1]) {
-			return true;
-		}
-	}
-	return false;
 };
